838c02e1901847788b6ce30c1d9df886a036a285
omrubiks <om.rubiks@gmail.com>
2022-03-27 11:09:18 +0530

    make it better

--- app.js
+++ app.js
@@ -7,9 +7,28 @@ const isAuth = require("./middleware/is-Auth");
 const graphqlSchema = require("./graphql/schema/index");
 const { rootResolver } = require("./graphql/resolvers/index");
 const { request } = require("express");
+const helmet = require("helmet");
+const compression = require("compression");
+const morgan = require("morgan");
+const fs = require("fs");
+const path = require("path");
+const https = require("https");
 
 const app = express();
 
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, "access.log"),
+  { flags: "a" }
+);
+
+// const privateKey = fs.readFileSync("server.key");
+// const certificate = fs.readFileSync("server.cert");
+
+// app.use(helmet());
+app.use(compression());
+app.use(morgan("combined", { stream: accessLogStream }));
+
+const MONGO_URL = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.kqn7m.mongodb.net/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority`;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(bodyParser.json());
@@ -36,11 +55,12 @@ app.use(
 );
 
 mongoose
-  .connect(
-    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.kqn7m.mongodb.net/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority`
-  )
+  .connect(MONGO_URL)
   .then(() => {
-    app.listen(5000);
+    // https
+    //   .createServer({ key: privateKey, cert: certificate }, app)
+    //   .listen(process.env.PORT || 5000);
+      app.listen(process.env.PORT || 5000);
     console.log("Connection Success");
   })
   .catch((err) => {
