9525d121b8c73e457003e034e1d4d9ac2466e766
omrubiks <om.rubiks@gmail.com>
2022-04-02 15:13:41 +0530

    meregedone

--- models/Orders.js
+++ models/Orders.js
@@ -21,46 +21,11 @@ address{
 }
 */
 
-<<<<<<< HEAD
 const OrdersSchema = new Schema({
   name: {
     firstName: {
       type: String,
       required: true
-=======
-const OrdersSchema = new Schema(
-  {
-    name: {
-      firstName: { type: String, required: true },
-      lastName: { type: String, required: true },
-    },
-
-    user: {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-    },
-
-    address: {
-      add1: { type: String, required: true },
-      landmarrk: { type: String },
-      street: { type: String },
-      city: { type: String, required: true },
-      state: { type: String, required: true },
-      // country: { type: String, required: true },
-      pincode: { type: Number, required: true },
-      phoneNo: { type: String, required: true },
-    },
-
-    orderProducts: [
-      {
-        type: Schema.Types.ObjectId,
-      },
-    ],
-    orderStatus: {
-      type: Boolean,
-      required: true,
-      default: false,
->>>>>>> e2fe32b832aecb68cde47d9915a92ab218306813
     },
     lastName: {
       type: String,
@@ -72,7 +37,6 @@ const OrdersSchema = new Schema(
     type: Number,
     required: true
   },
-
 
   user: {
     type: Schema.Types.ObjectId,
